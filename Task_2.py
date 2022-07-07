4192f1986b9543faa1079bfea36fae09af50a3c0
Oleg S <leshiyoleg@mail.ru>
2022-07-07 23:21:01 +0300

    Initial commit Added Tasks_1-7 from Seminar_1

---
+++ Task_2.py
@@ -0,0 +1,21 @@
+#  2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+    
+#      Примеры:
+    
+#      - 1, 4, 8, 7, 5 -> 8
+#      - 78, 55, 36, 90, 2 -> 90
+
+# print ('Введите 5 чисел через клавишу Enter')
+import random
+list_numbers = []
+
+for i in range(5):
+    list_numbers.append(random.randint(1,100))
+print(list_numbers)
+
+# list_numbers = [1,2,3,4,5]
+max_num = list_numbers[0]
+for item in list_numbers:
+    if max_num < item:
+        max_num = item
+print(max_num)
